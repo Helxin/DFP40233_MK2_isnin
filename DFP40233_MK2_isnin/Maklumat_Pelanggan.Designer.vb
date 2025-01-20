d2982b5b0cf77bed370be60cfced9bc32dece748
Asperl <Asperl@LAPTOP-7AF5V6V8>
2025-01-20 12:13:48 +0800

    Add project files.

---
+++ DFP40233_MK2_isnin/Maklumat_Pelanggan.Designer.vb
@@ -0,0 +1,398 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Maklumat_Pelanggan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        lblNama = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        TextBox3 = New TextBox()
+        Label2 = New Label()
+        ComboBox1 = New ComboBox()
+        Label3 = New Label()
+        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
+        GroupBox1 = New GroupBox()
+        rdoPerempuan = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        CheckBox1 = New CheckBox()
+        GroupBox3 = New GroupBox()
+        CheckBox4 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        DateTimePicker1 = New DateTimePicker()
+        DataGridView1 = New DataGridView()
+        Bil = New DataGridViewTextBoxColumn()
+        Nama = New DataGridViewTextBoxColumn()
+        no_telefon = New DataGridViewTextBoxColumn()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Label4 = New Label()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' lblNama
+        ' 
+        lblNama.AutoSize = True
+        lblNama.Location = New Point(43, 111)
+        lblNama.Name = "lblNama"
+        lblNama.Size = New Size(59, 25)
+        lblNama.TabIndex = 0
+        lblNama.Text = "Nama"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(117, 111)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(406, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(117, 163)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(406, 161)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(43, 163)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Alamat"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(117, 344)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(406, 31)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(10, 344)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(101, 25)
+        Label2.TabIndex = 4
+        Label2.Text = "No. Telefon"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Johor  ", "Negeri Perak  ", "Negeri Melaka  ", "Negeri Sembilan  ", "Pulau Pinang  ", "Sabah  ", "Kedah  ", "Selangor  ", "Perlis  ", "Sarawak  ", "Pahang  ", "Terengganu  ", "Kelantan  ", "Labuan  "})
+        ComboBox1.Location = New Point(117, 406)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(182, 33)
+        ComboBox1.TabIndex = 6
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(10, 406)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(64, 25)
+        Label3.TabIndex = 7
+        Label3.Text = "Negeri"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(rdoPerempuan)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(572, 111)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(300, 150)
+        GroupBox1.TabIndex = 8
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Jantina"
+        ' 
+        ' rdoPerempuan
+        ' 
+        rdoPerempuan.AutoSize = True
+        rdoPerempuan.Location = New Point(19, 92)
+        rdoPerempuan.Name = "rdoPerempuan"
+        rdoPerempuan.Size = New Size(126, 29)
+        rdoPerempuan.TabIndex = 1
+        rdoPerempuan.TabStop = True
+        rdoPerempuan.Text = "Perempuan"
+        rdoPerempuan.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(19, 48)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(80, 29)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Lelaki"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton2)
+        GroupBox2.Controls.Add(RadioButton3)
+        GroupBox2.Location = New Point(572, 289)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(300, 150)
+        GroupBox2.TabIndex = 9
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Kaedah Pembayaran"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(19, 92)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(104, 29)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Cashless"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(19, 48)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(75, 29)
+        RadioButton3.TabIndex = 0
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Cash"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(19, 46)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(73, 29)
+        CheckBox1.TabIndex = 10
+        CheckBox1.Text = "Milo"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(CheckBox4)
+        GroupBox3.Controls.Add(CheckBox3)
+        GroupBox3.Controls.Add(CheckBox2)
+        GroupBox3.Controls.Add(CheckBox1)
+        GroupBox3.Location = New Point(897, 111)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(300, 150)
+        GroupBox3.TabIndex = 11
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Perisa"
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(149, 97)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(90, 29)
+        CheckBox4.TabIndex = 13
+        CheckBox4.Text = "Pisang"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(149, 46)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(117, 29)
+        CheckBox3.TabIndex = 12
+        CheckBox3.Text = "Strawberri"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(19, 97)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(74, 29)
+        CheckBox2.TabIndex = 11
+        CheckBox2.Text = "Kopi"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(897, 289)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(300, 31)
+        DateTimePicker1.TabIndex = 12
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToOrderColumns = True
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Bil, Nama, no_telefon})
+        DataGridView1.Location = New Point(572, 465)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 62
+        DataGridView1.Size = New Size(625, 334)
+        DataGridView1.TabIndex = 13
+        ' 
+        ' Bil
+        ' 
+        Bil.HeaderText = "Bil"
+        Bil.MinimumWidth = 8
+        Bil.Name = "Bil"
+        Bil.Width = 150
+        ' 
+        ' Nama
+        ' 
+        Nama.HeaderText = "Nama"
+        Nama.MinimumWidth = 8
+        Nama.Name = "Nama"
+        Nama.Width = 150
+        ' 
+        ' no_telefon
+        ' 
+        no_telefon.HeaderText = "No. Telefon"
+        no_telefon.MinimumWidth = 8
+        no_telefon.Name = "no_telefon"
+        no_telefon.Width = 150
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(117, 500)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 14
+        Button1.Text = "Create"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(117, 540)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 15
+        Button2.Text = "Update"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(235, 500)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(112, 34)
+        Button3.TabIndex = 16
+        Button3.Text = "Read"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(235, 540)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(112, 34)
+        Button4.TabIndex = 17
+        Button4.Text = "Create"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Black", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(257, 21)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(636, 65)
+        Label4.TabIndex = 18
+        Label4.Text = "MAKLUMAT PELANGGAN"
+        ' 
+        ' Maklumat_Pelanggan
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1235, 870)
+        Controls.Add(Label4)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label3)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(lblNama)
+        Name = "Maklumat_Pelanggan"
+        Text = "Maklumat Pelanggan"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblNama As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rdoPerempuan As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Bil As DataGridViewTextBoxColumn
+    Friend WithEvents Nama As DataGridViewTextBoxColumn
+    Friend WithEvents no_telefon As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ DFP40233_MK2_isnin/Maklumat_Pelanggan.vb
@@ -0,0 +1,5 @@
+﻿Public Class Maklumat_Pelanggan
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNama.Click
+
+    End Sub
+End Class
